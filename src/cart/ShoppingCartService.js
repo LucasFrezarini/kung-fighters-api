cfeb04b06841947c27381cf6c9367217206cb424
Lucas Frezarini <lucas.frezarini@hypnobox.com.br>
2018-05-08 14:21:39 -0300

    Trocando o push por addToSet para evitar duplicatas de itens no carrinho de compras

--- src/cart/ShoppingCartService.js
+++ src/cart/ShoppingCartService.js
@@ -2,14 +2,15 @@ import Client from "../database/mongo/models/Client";
 
 class ShoppingCartService {
   async addProductToCart(id, product, quantity) {
+    const exists = Client.findById(id).shoppingCart.items;
     return Client.findByIdAndUpdate(id, {
-      $push: { 
+      $addToSet: { 
         "shoppingCart.items": {
           product: product,
           quantity: quantity
         } 
       }
-    });
+    }).exec();
   }
 
   async getCartByClientId(id) {
